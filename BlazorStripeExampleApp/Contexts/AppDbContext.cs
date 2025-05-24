f66c8d627330930a98eb5c05614278ff288c8cfe
patrikduch <duchpatrik@icloud.com>
2025-05-24 15:43:19 +0200

    feat:  EFCore integration

---
+++ BlazorStripeExampleApp/Contexts/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorStripeExample.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorStripeExample.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Payment> Payments => Set<Payment>();
+    }
+}
---
+++ BlazorStripeExampleApp/Entities/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorStripeExample.Entities;
+
+public class Payment
+{
+    public int Id { get; set; }
+    public string SessionId { get; set; } = null!;
+    public string CustomerEmail { get; set; } = null!;
+    public long AmountTotal { get; set; }
+    public string Currency { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
