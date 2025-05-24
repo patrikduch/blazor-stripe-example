032292695c254f78553d3c310099d793641a4ec3
patrikduch <duchpatrik@icloud.com>
2025-05-24 16:35:13 +0200

    feat: add payment migration

---
+++ BlazorStripeExampleApp/Migrations/20250524142833_AddPaymentTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BlazorStripeExample.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SessionId = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CustomerEmail = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    AmountTotal = table.Column<long>(type: "bigint", nullable: false),
+                    Currency = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payment");
+        }
+    }
+}
