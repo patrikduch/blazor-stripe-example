512bf084c8a4fab1e9dc72a157416fd4201eec08
patrikduch <duchpatrik@icloud.com>
2025-05-30 23:48:38 +0200

    feat: pricing consts

---
+++ BlazorStripeExampleApp/Consts/PricingConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorStripeExample.Consts;
+
+public class PricingConsts
+{
+    public const string DefaultBillingInterval = "monthly";
+    public const decimal DefaultMonthlyPrice = 29m;
+    public const decimal DefaultYearlyPrice = 290m;
+}
